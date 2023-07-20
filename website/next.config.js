1438dafd300f32726a6cd5d5acd5a9861498b501
Winbert Zhang <winbertzhang@gmail.com>
2023-07-20 13:58:23 -0700

    Deploying Nextjs

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-module.exports = nextConfig
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
