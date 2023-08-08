f94829b1d8fce3b44216101695699dc3aa48cc0c
Winbert Zhang <winbertzhang@gmail.com>
2023-08-08 12:55:59 -0700

    Staff Page Bios [UNFINISHED]

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
   theme: {
     fontSize: {
       sm: "0.8rem",
+      md: "0.9rem",
       base: "1rem",
+      lg: "1.125rem",
       xl: "1.25rem",
       "2xl": "1.563rem",
       "3xl": "1.953rem",
