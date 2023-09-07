6f38720690b6cb804bfa0a0427e339b41e857a4d
Winbert Zhang <winbertzhang@gmail.com>
2023-09-07 00:57:06 -0700

    Add Google Calendar Embed

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,6 +1,8 @@
 import NavBar from "../_components/navbar";
 import Header from "../_components/header";
 import Link from "next/link";
+import Iframe from "react-iframe";
+import Footer from "../_components/footer";
 
 const page = () => {
   return (
@@ -8,10 +10,19 @@ const page = () => {
       <Header title="UCSB TASA"></Header>
       <NavBar></NavBar>
       <div className="bg-gray-100">
-        <div className="flex justify-center">
-          <h1>Upcoming Events</h1>
+        <div className="text-center justify-center mx-4 lg:mx-16 pt-4">
+          <h1>Upcoming Events!</h1>
+          <div className="flex justify-center">
+            <Iframe
+              url="https://calendar.google.com/calendar/embed?src=7hptoesel85ltt2ndeg0dtptec%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+              width="800"
+              height="800"
+              className="mb-8 border-solid 1px border-[#777]"
+            ></Iframe>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
