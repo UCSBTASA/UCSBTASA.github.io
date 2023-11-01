ee0b59cf998b40a9ba18e98a04bf64f11f084220
Winbert Zhang <winbertzhang@gmail.com>
2023-10-31 23:32:35 -0700

    Fix Events + Family Trees

--- app/family-trees/page.tsx
+++ app/family-trees/page.tsx
@@ -4,103 +4,103 @@ import Header from "../_components/header";
 import Link from "next/link";
 import Footer from "../_components/footer";
 import Image from "next/image";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const FamilyTrees = () => {
-    const trees = [
-      {
-        title: "HuFuWu, Helen's Line",
-        image: "/famTrees_photos/hufuwu_1.png",
-        slug: "hufuwu",
-      },
-      {
-        title: "HuFuWu, Casidy's Line",
-        image: "/famTrees_photos/hufuwu_2.png",
-        slug: "hufuwu",
-      },
-      {
-        title: "Flower",
-        image: "/famTrees_photos/flower.png",
-        slug: "flower",
-      },
-      {
-        title: "Mogu Mogu",
-        image: "/famTrees_photos/mogu_mogu.png",
-        slug: "mogu-mogu",
-      },
-      {
-        title: "Top 5",
-        image: "/famTrees_photos/top_5.png",
-        slug: "top-5",
-      },
-      {
-        title: "Elite",
-        image: "/famTrees_photos/elite.png",
-        slug: "elite",
-      },
-      {
-        title: "Python",
-        image: "/famTrees_photos/python.png",
-        slug: "python",
-      },
-      {
-        title: "Kangaroo",
-        image: "/famTrees_photos/kangaroo.png",
-        slug: "kangaroo",
-      },
-      {
-        title: "Dino",
-        image: "/famTrees_photos/dino.png",
-        slug: "dino",
-      },
-      {
-        title: "Cat",
-        image: "/famTrees_photos/cat.png",
-        slug: "cat",
-      },
-      {
-        title: "Oolong",
-        image: "/famTrees_photos/oolong.png",
-        slug: "oolong",
-      },
-      {
-        title: "TT",
-        image: "/famTrees_photos/tt.png",
-        slug: "tt",
-      },
-      {
-        title: "Dragon",
-        image: "/famTrees_photos/dragon.png",
-        slug: "dragon",
-      },
-      {
-        title: "Best",
-        image: "/famTrees_photos/best.png",
-        slug: "best",
-      },
-      {
-        title: "Seal",
-        image: "/famTrees_photos/seal.png",
-        slug: "seal",
-      },
-      {
-        title: "Van",
-        image: "/famTrees_photos/van.png",
-        slug: "van",
-      },
-      // {
-      //   title: "",
-      //   image: "/famTrees_photos/",
-      //   slug: "",
-      // },
-    ];
-    const [isImageClicked, setImageClicked] = useState(false);
-    const [clickedImage, setClickedImage] = useState<string | null>(null);
-  
-    const handleImageClick = (imageSrc: string) => {
-      setImageClicked(!isImageClicked);
-      setClickedImage(imageSrc);
-    };
+  const trees = [
+    {
+      title: "HuFuWu, Helen's Line",
+      image: "/famTrees_photos/hufuwu_1.png",
+      slug: "hufuwu",
+    },
+    {
+      title: "HuFuWu, Casidy's Line",
+      image: "/famTrees_photos/hufuwu_2.png",
+      slug: "hufuwu",
+    },
+    {
+      title: "Flower",
+      image: "/famTrees_photos/flower.png",
+      slug: "flower",
+    },
+    {
+      title: "Mogu Mogu",
+      image: "/famTrees_photos/mogu_mogu.png",
+      slug: "mogu-mogu",
+    },
+    {
+      title: "Top 5",
+      image: "/famTrees_photos/top_5.png",
+      slug: "top-5",
+    },
+    {
+      title: "Elite",
+      image: "/famTrees_photos/elite.png",
+      slug: "elite",
+    },
+    {
+      title: "Gremlin",
+      image: "/famTrees_photos/gremlin.png",
+      slug: "gremlin",
+    },
+    {
+      title: "Kangaroo",
+      image: "/famTrees_photos/kangaroo.png",
+      slug: "kangaroo",
+    },
+    {
+      title: "Dino",
+      image: "/famTrees_photos/dino.png",
+      slug: "dino",
+    },
+    {
+      title: "Cat",
+      image: "/famTrees_photos/cat.png",
+      slug: "cat",
+    },
+    {
+      title: "Oolong",
+      image: "/famTrees_photos/oolong.png",
+      slug: "oolong",
+    },
+    {
+      title: "TT",
+      image: "/famTrees_photos/tt.png",
+      slug: "tt",
+    },
+    {
+      title: "Dragon",
+      image: "/famTrees_photos/dragon.png",
+      slug: "dragon",
+    },
+    {
+      title: "Best",
+      image: "/famTrees_photos/best.png",
+      slug: "best",
+    },
+    {
+      title: "Seal",
+      image: "/famTrees_photos/seal.png",
+      slug: "seal",
+    },
+    {
+      title: "Van",
+      image: "/famTrees_photos/van.png",
+      slug: "van",
+    },
+    // {
+    //   title: "",
+    //   image: "/famTrees_photos/",
+    //   slug: "",
+    // },
+  ];
+  const [isImageClicked, setImageClicked] = useState(false);
+  const [clickedImage, setClickedImage] = useState<string | null>(null);
+
+  const handleImageClick = (imageSrc: string) => {
+    setImageClicked(!isImageClicked);
+    setClickedImage(imageSrc);
+  };
 
   return (
     <div>
@@ -148,6 +148,5 @@ const FamilyTrees = () => {
     </div>
   );
 };
-  
-  export default FamilyTrees;
-  
+
+export default FamilyTrees;
