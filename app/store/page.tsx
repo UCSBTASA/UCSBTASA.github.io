782a26cf31459f0bcb64a78aab89f1ecfbff30e0
smmzhu <smmzhu@gmail.com>
2024-01-01 20:09:25 -0800

    store is done

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -10,12 +10,14 @@ const Store = () => {
     {
       name: "TASA Hoodie 2023",
       price: 35.0,
-      imageSrc: "/store/merch_2023.png",
+      imageFront: "/store/hoodiefront2023.jpg",
+      imageBack: "/store/hoodieback2023.jpg",
     },
     {
       name: "TASA T-Shirt 2023",
       price: 20.0,
-      imageSrc: "/store/merch_tshirt.png",
+      imageFront: "/store/tshirtfront2023.jpg",
+      imageBack: "/store/tshirtback2023.jpg",
     },
     // {
     //   name: "Keychain 1",
@@ -34,29 +36,41 @@ const Store = () => {
       <Header title="UCSB TASA" />
       <NavBar />
       <></>
-      {/* <div className="px-4 py-8 text-center w-screen bg-gray-100">
+      <div className="px-4 py-8 text-center w-screen bg-gray-100 flex flex-col items-center">
         <h2 className="text-3xl font-bold text-gray-900 uppercase">Store</h2>
         <p className="text-gray-600">
           If you'd like to purchase any of our merch, please fill out the order
           form and we'll reach out to you about pickup options!
         </p>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+        <div className="gap-4 mt-4 flex flex-row justify-center">
           {products.map((product, index) => (
             <div key={index} className="bg-white p-4 border rounded shadow">
-              <Image
+              {/* <Image
                 src={product.imageSrc}
                 alt={product.name}
                 width={300}
                 height={300}
                 style={{ display: "inline" }}
-              />
+              /> */}
+              {/* <div className="bg-[url('/store/hoodiefront2023.jpg')] bg-cover bg-center hover:bg-[url('/store/hoodieback2023.jpg')] h-72" ></div> */}
+              <div className="h-80 w-80 relative group">
+                <div className="h-80 w-80 bg-center bg-no-repeat bg-contain group-hover:opacity-0 transition-opacity duration-200 ease-in-out" style = {{backgroundImage: `url(${product.imageFront})`}}></div>
+                <div className="h-80 w-80 opacity-0 group-hover:opacity-100 bg-center bg-no-repeat bg-contain absolute top-0 left-0 transition-opacity duration-200 ease-in-out" style = {{backgroundImage: `url(${product.imageBack})`}}></div>
+              </div>
               <h3 className="text-lg font-semibold">{product.name}</h3>
               <p className="text-gray-600">${product.price.toFixed(2)}</p>
             </div>
           ))}
         </div>
-      </div> */}
-      <Construction></Construction>
+        <a 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-36" 
+          href = "https://docs.google.com/forms/d/e/1FAIpQLSfH1px0y8ROK-UcSzHm3G9h6gG3MP8vtugXhkc5iTV6hGDTag/viewform"
+          target= "_blank"
+          rel = "noreferrer noopener">
+          Order Now
+        </a>  
+      </div>
+      {/* <Construction></Construction> */}
       <Footer></Footer>
     </div>
   );
