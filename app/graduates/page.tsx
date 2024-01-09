158aca6ee018b7770b3383c9656f75b5a0aafc14
Ajay Liu <ajayliu2928@gmail.com>
2024-01-09 02:28:17 -0800

    INCOMPLETE - can't get sheets to work yet

--- app/graduates/page.tsx
+++ app/graduates/page.tsx
@@ -1,11 +1,30 @@
-import Construction from "../../components/under_construction";
+import Head from "next/head";
+import { getAlumniSheet } from "../../libs/sheets";
 
-const Graduates = () => {
+interface Alumni {
+  name: string;
+  year: string;
+  bio: string;
+  linkedin: string;
+}
+export default function IndexPage({ alumni }: { alumni: Alumni[] }) {
   return (
-    <div>
-      <Construction></Construction>
-    </div>
+    <>
+      <Head>
+        <title>Title - FrasNym</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {alumni[0].name}
+    </>
   );
-};
+}
 
-export default Graduates;
+export async function getStaticProps(context) {
+  const alumni = await getAlumniSheet();
+  return {
+    props: {
+      alumni: alumni.slice(1, alumni.length), // remove sheet header
+    },
+    revalidate: 1, // In seconds
+  };
+}
