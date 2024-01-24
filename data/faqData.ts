6a6052af28af08653417e22697e7fe5b27d34a22
WinbertZhang <winbertzhang@gmail.com>
2024-01-24 01:40:13 -0800

    Update Nav Data and FAQ

--- data/faqData.ts
+++ data/faqData.ts
@@ -1,12 +1,29 @@
 const faqData = [
-    {
-        question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros arcu, mollis vitae sapien et, gravida cursus mi.?",
-        answer: "Ut eget consectetur sapien, facilisis pretium nunc. In mattis eget sem eu porttitor. Aliquam in varius ligula, non gravida arcu.",
-    },
-    {
-        question: "Pellentesque vulputate nibh ut egestas dictum. Maecenas fringilla vulputate leo, sed convallis lorem tempus vitae?",
-        answer: "blah blah blahProin vulputate eu urna sed consequat. Donec ex velit, ornare at tellus non, mollis feugiat lectus. Sed vehicula suscipit laoreet. Donec pharetra dui quis tincidunt gravida. Aliquam imperdiet quam egestas, scelerisque erat a, scelerisque mi. Sed rutrum bibendum libero eu bibendum",
-    }
+  {
+    question: "Do I need to be Taiwanese to join UCSB TASA?",
+    answer:
+      "No, UCSB TASA is open to students of all backgrounds! Our organization thrives on the diversity of its members. Whether you're interested in Taiwanese culture, seeking to learn more about Taiwanese American heritage, or simply looking for a vibrant community, you're welcome here. We celebrate and share the rich Taiwanese culture in an inclusive environment.",
+  },
+  {
+    question: "What are the membership fees for UCSB TASA?",
+    answer:
+      "UCSB does not have any membership fees! Anyone is welcome to join our open events and meetings.",
+  },
+  {
+    question: "What is the best way to stay updated on everything TASA?",
+    answer:
+      "The best way to keep up with UCSB TASA is through our Instagram and Linktree on the homepage! We also maintain an event calendar on our website, featuring details about general meetings, special events, and more!",
+  },
+  {
+    question: "What are fams in TASA and how does the system work?",
+    answer:
+      "TASA fams are smaller groups of bigs and littles that are formed at the beginning of each school year. Fams are a great way to meet new people and make friends! Each fam will host their own events and activities throughout the year. It's a great way to get involved in TASA and meet new people!",
+  },
+  {
+    question: "What's the difference between TSA and UCSB TASA?",
+    answer:
+      "While both organizations embrace Taiwanese culture, TASA focuses more on the Taiwanese American experience, catering to students interested in Taiwanese heritage and culture. TSA, or Taiwanese Student Association, primarily serves as a community for Taiwanese students at UCSB.",
+  },
 ];
 
 export default faqData;
