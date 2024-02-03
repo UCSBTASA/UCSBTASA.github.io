f49b8779873162accc818c369703245e56dcb47b
MeganCFu <meganthefu@gmail.com>
2024-02-02 17:03:19 -0800

    Update fakecarousel.tsx

--- components/fakecarousel.tsx
+++ components/fakecarousel.tsx
@@ -27,22 +27,25 @@ const FakeCarousel = () => {
     return(
         <div className =  "bg-gray-100 flex justify-center items-center">
             <ScrollTransition>
-                <div className = "flex justify-center items-center m-20 px-60 py-32" >
+                {/* <div className = "flex justify-center items-center m-10">
+                </div> */}
+                <div className = "flex justify-center items-center m-20 px-64 py-36" >
                     <Image  
                         src={`/carousel/${images[index]}`}
                         alt="Carousel"
                         fill
                         style={{objectFit:"cover", objectPosition: "center"}}
                         onLoadingComplete = {onLoadCallback}
-                        className="rounded-xl"
+                        className="rounded-3xl"
                     />                
                </div>
+               <button className="absolute inset-y-0 right-0 px-32 rounded-e-3xl bg-gray-600 opacity-5 hover:opacity-0"
+                    onClick={() => setIndex(index < 6 ? index + 1 : 0)}></button>
+                    
+                <button className="absolute inset-y-0 left-0 px-32 rounded-s-3xl bg-gray-600 opacity-5 hover:opacity-0"
+                onClick={() => setIndex(index > 0 ? index - 1 : 6)}></button>       
 
-                <button className="absolute inset-y-0 right-0 p-40 rounded-xl bg-gray-600 opacity-10 hover:opacity-5"
-                onClick={() => setIndex(index < 6 ? index + 1 : 0)}></button>
-                
-                <button className="absolute inset-y-0 left-0 p-40 rounded-xl bg-gray-600 opacity-10 hover:opacity-5"
-                onClick={() => setIndex(index > 0 ? index - 1 : 6)}></button>
+
                 </ScrollTransition>
             </div>
     )
