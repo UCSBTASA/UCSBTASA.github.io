983164054e27a412b6e51c41fadd7abc577f5e4b
Justin Lang <jlang61@ucsb.edu>
2024-02-02 17:32:50 -0800

    updating the script for the eventData

--- data/event_automation/script_2.py
+++ data/event_automation/script_2.py
@@ -21,12 +21,11 @@ gc = gspread.service_account(filename = ".config/service_account.json")
 sh = gc.open("TASA Scheduling 23-24")
 
 # Get the current sheet of interest and its records
-sheet_instance = sh.get_worksheet(0)
+sheet_instance = sh.get_worksheet(2)
 records_data = sheet_instance.get_all_records()
 
 # Convert to pandas DataFrame
 records_df = pd.DataFrame.from_dict(records_data)
-    
 # Convert DataFrame to JSON format
 events = []
 for index, row in records_df.iterrows():
