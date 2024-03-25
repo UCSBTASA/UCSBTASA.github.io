dea661b684e10e99b4f3f96a04a31de65bea5ecd
Justin Lang <jlang61@ucsb.edu>
2024-03-25 03:52:17 +0000

    Update eventData.ts

---
+++ eventData.ts
@@ -0,0 +1,119 @@
+[
+  {
+    "title": "Speed Dating",
+    "day of week": "Friday",
+    "date": "2024-01-19",
+    "time": "8-9:30PM",
+    "location": "Lot 22/ Phelps 1160",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "GM #1",
+    "day of week": "Monday",
+    "date": "2024-01-22",
+    "time": "8-10PM",
+    "location": "TD-W 1701",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Field Day",
+    "day of week": "Friday",
+    "date": "2024-01-26",
+    "time": "3-5PM",
+    "location": "Student Health Lawn",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TFTI",
+    "day of week": "Saturday",
+    "date": "2024-01-27",
+    "time": "Overnight",
+    "location": "UCLA (Mesa Lawn)",
+    "customTime": "Overnight",
+    "ready": "Yes"
+  },
+  {
+    "title": "Winter Big/Little Reveal",
+    "day of week": "Sunday",
+    "date": "2024-02-04",
+    "time": "2-4PM",
+    "location": "Top of Lot 22",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "GM #2",
+    "day of week": "Monday",
+    "date": "2024-02-05",
+    "time": "8-10PM",
+    "location": "TD-W 1701",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Online Game Night\n",
+    "day of week": "Wednesday",
+    "date": "2024-02-07",
+    "time": "8:00 PM",
+    "location": "Online (Discord)",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Mahjong Mocktails",
+    "day of week": "Thursday",
+    "date": "2024-02-08",
+    "time": "8-10PM ",
+    "location": "Girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "NSU x TASA",
+    "day of week": "Friday",
+    "date": "2024-02-09",
+    "time": "5-7; 9-12AM",
+    "location": "Girvetz 1004",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Lunar New Year Hotpot Night!",
+    "day of week": "Saturday",
+    "date": "2024-02-10",
+    "time": "6-9PM",
+    "location": "Multi Cultural Center",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-02-15",
+    "time": "8-10PM",
+    "location": "Girvetz 1116",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "winbert",
+    "day of week": "sunday",
+    "date": "2024-03-09",
+    "time": "1-5 AM ",
+    "location": "Online",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "testing",
+    "day of week": "monday",
+    "date": "2024-03-10",
+    "time": "1-5 AM ",
+    "location": "Online",
+    "customTime": "",
+    "ready": "Yes"
+  }
+]
