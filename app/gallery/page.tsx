c90033c6094aa7b90154f1519df9024591279630
Justin Lang <jlang61@ucsb.edu>
2024-03-29 00:37:19 -0700

    adding gallery filters - could work for quarters

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import {
   galleryImages2023_2024,
   galleryImages2022_2023,
@@ -5,10 +7,44 @@ import {
   galleryImages2019_2020,
   galleryImages2018_2019,
   galleryImages2017_2018,
+  GalleryImage,
 } from "@/data/galleryImages";
 import GalleryYearSection from "@/components/galleryYearSection";
-
+import { Button } from "@/components/ui/button";
+import { ESLINT_DEFAULT_DIRS } from "next/dist/lib/constants";
 const GalleryPage = () => {
+  // State to manage active years
+  const [activeYears, setActiveYears] = useState(["2023-2024"]);
+
+  // Handler function to toggle active years
+  const handleYearClick = (year: string) => {
+    console.log(year)
+    if (activeYears.includes(year)) {
+      // Year is already active, remove it
+      setActiveYears(activeYears.filter((activeYear) => activeYear !== year));
+    } else {
+      // Year is not active, add it
+      setActiveYears([...activeYears, year]);
+    }
+    console.log("Active years = ", activeYears);
+  };
+  function getGalleryImagesForYear(year: any): GalleryImage[] {
+    if (year == "2023-2024") {
+      return galleryImages2023_2024;
+    } else if (year == "2022-2023") {
+      return galleryImages2022_2023;
+    } else if (year == "2021-2022") {
+      return galleryImages2021_2022;
+    } else if (year == "2019-2020") {
+      return galleryImages2019_2020;
+    } else if (year == "2018-2019") {
+      return galleryImages2018_2019;
+    } else if (year == "2017-2018") {
+      return galleryImages2017_2018;
+    } else {
+      return galleryImages2023_2024;
+    }
+  }
   return (
     <div className="gallery-container ">
       <div className="bg-gray-100">
@@ -16,30 +52,51 @@ const GalleryPage = () => {
           <h2 className="text-4xl mt-4 font-semibold text-center">
             Our Favorite Memories
           </h2>
-          <GalleryYearSection
-            images={galleryImages2023_2024.slice().reverse()}
-            year="2023-2024"
-          />
-          <GalleryYearSection
-            images={galleryImages2022_2023}
-            year="2022-2023"
-          />
-          <GalleryYearSection
-            images={galleryImages2021_2022}
-            year="2021-2022"
-          />
-          <GalleryYearSection
-            images={galleryImages2019_2020}
-            year="2019-2020"
-          />
-          <GalleryYearSection
-            images={galleryImages2018_2019}
-            year="2018-2019"
-          />
-          <GalleryYearSection
-            images={galleryImages2017_2018}
-            year="2017-2018"
-          />
+          <div className="flex justify-center grid-cols-6 mx-auto">
+            <Button
+              onClick={() => handleYearClick("2023-2024")}
+              className="col-span-1 bg-gray-200 mx-auto"
+            >
+              2023-2024
+            </Button>
+            <Button
+              onClick={() => handleYearClick("2022-2023")}
+              className="col-span-1 bg-gray-200 mx-auto"
+            >
+              2022-2023
+            </Button>
+            <Button
+              onClick={() => handleYearClick("2021-2022")}
+              className="col-span-1 bg-gray-200 mx-auto"
+            >
+              2021-2022
+            </Button>
+            <Button
+              onClick={() => handleYearClick("2019-2020")}
+              className="col-span-1 bg-gray-200 mx-auto"
+            >
+              2019-2020
+            </Button>
+            <Button
+              onClick={() => handleYearClick("2018-2019")}
+              className="col-span-1 bg-gray-200 mx-auto"
+            >
+              2018-2019
+            </Button>
+            <Button
+              onClick={() => handleYearClick("2017-2018")}
+              className="col-span-1 bg-gray-200 mx-auto"
+            >
+              2017-2018
+            </Button>
+          </div>
+          {activeYears.map((year) => (
+            <GalleryYearSection
+              key={year}
+              images={getGalleryImagesForYear(year)}
+              year={year}
+            />
+          ))}
         </div>
       </div>
     </div>
