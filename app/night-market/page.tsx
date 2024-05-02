6d1c3abb1b030edd478b909d217a5740893c491f
Samuel Zhu <shmeelboi@Samuels-MacBook-Pro.local>
2024-05-01 21:25:58 -0700

    fixed mobile problem

--- app/night-market/page.tsx
+++ app/night-market/page.tsx
@@ -1,4 +1,21 @@
+'use client'
 import Stand from '../../components/nmstand';
+import {use100vh} from 'react-div-100vh'
+
+function detectDevice() { 
+  if (navigator.userAgent.match(/Android/i) 
+    || navigator.userAgent.match(/webOS/i) 
+    || navigator.userAgent.match(/iPhone/i) 
+    || navigator.userAgent.match(/iPad/i) 
+    || navigator.userAgent.match(/iPod/i) 
+    || navigator.userAgent.match(/BlackBerry/i) 
+    || navigator.userAgent.match(/Windows Phone/i) 
+  ) { 
+    return 'phone'; 
+  } else { 
+    return 'computer'; 
+  } 
+} 
 
 const StandInfo = [
   {name: "CSU", x: 42.5, y: 32.5, height: 6, width: 9, blurb: "lorum ipsum dolor sit amet"},
@@ -41,9 +58,15 @@ const graphicSources = [
   "url(nm_visuals/img5.jpeg)",
 ]
 
+const isPhone = (detectDevice() == 'phone');
+
 
 const NightMarket = () => {
+  const vh100 = use100vh();
+  console.log(vh100)
+  console.log(isPhone)
   return (
+    !isPhone ? 
     <div className="px-4 py-8 text-center w-screen h-auto bg-gray-100 flex-col flex items-center ">
       <h2 className="text-3xl font-bold text-gray-900 uppercase">Night Market</h2>
         <p className="text-gray-600">
@@ -53,7 +76,19 @@ const NightMarket = () => {
             {StandInfo.map((stand) => <Stand color = {stand.color} name={stand.name} x={stand.x} y={stand.y} height={stand.height} width={stand.width} blurb={stand.blurb}/>)}
         </div> */}
         {graphicSources.map((source) => <div className = "bg-cover m-10 rounded-2xl	drop-shadow-[0_35px_35px_rgba(0,0,0,0.5)]" style={{ backgroundImage: source, width: '90vh', height: '90vh', backgroundRepeat: "no-repeat"}}></div>)}
-        <div className = "bg-cover m-10 rounded-2xl	drop-shadow-[0_35px_35px_rgba(0,0,0,0.5)]" style={{ backgroundImage: "url(nm_visuals/img0.jpeg)", width: '70vh', height: '90vh', backgroundRepeat: "no-repeat"}}></div>
+        <div className = "bg-cover m-10 rounded-2xl	drop-shadow-[0_35px_35px_rgba(0,0,0,0.5)]" style={{ backgroundImage: "url(nm_visuals/img0.jpeg)", width: '90vh', height: '115vh', backgroundRepeat: "no-repeat"}}></div> 
+    </div>
+    : 
+    <div className="px-4 py-8 text-center w-screen h-auto bg-gray-100 flex-col flex items-center ">
+      <h2 className="text-3xl font-bold text-gray-900 uppercase">Night Market</h2>
+        <p className="text-gray-600">
+          Come to our Night Market to see all the amazing clubs and vendors we have to offer!
+        </p>
+        {/* <div className = "flex absolute bg-cover rounded-2xl	drop-shadow-[0_35px_35px_rgba(0,0,0,0.5)]" style={{ backgroundImage: "url(nm_visuals/nm_map.png)", width: '90vh', height: '90vh', backgroundRepeat: "no-repeat"}}>
+            {StandInfo.map((stand) => <Stand color = {stand.color} name={stand.name} x={stand.x} y={stand.y} height={stand.height} width={stand.width} blurb={stand.blurb}/>)}
+        </div> */}
+        {graphicSources.map((source) => <div className = "bg-cover m-10 rounded-2xl	drop-shadow-[0_35px_35px_rgba(0,0,0,0.5)]" style={{ backgroundImage: source, width: vh100 ? vh100*0.25 : '50vh', height: vh100 ? vh100*0.25 : '50vh', backgroundRepeat: "no-repeat"}}></div>)}
+        <div className = "bg-cover m-10 rounded-2xl	drop-shadow-[0_35px_35px_rgba(0,0,0,0.5)]" style={{ backgroundImage: "url(nm_visuals/img0.jpeg)", width: vh100 ? vh100*0.25 : '50vh', height: vh100 ? vh100*0.25*9/7 : '50vh', backgroundRepeat: "no-repeat"}}></div> 
     </div>
   );
 };
