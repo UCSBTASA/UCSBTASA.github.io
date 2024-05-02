e7269a8f0cd8e895bb3dfe770a032bedbf2dfd54
Samuel Zhu <shmeelboi@Samuels-MacBook-Pro.local>
2024-05-01 21:40:54 -0700

    testing why vercel isnt deploying

--- data/navbar.ts
+++ data/navbar.ts
@@ -1,6 +1,5 @@
 export const navLinks = [
   { href: "/", name: "Home" },
-  { href: "/night-market", name: "Night Market" },
   { href: "/programs", name: "Programs" },
   { href: "/staff", name: "Staff" },
   { href: "/events", name: "Events" },
@@ -9,6 +8,7 @@ export const navLinks = [
   { href: "/contact", name: "Contact" },
   { href: "/faq", name: "FAQ" },
   { href: "/store", name: "Store" },
+  { href: "/night-market", name: "Night Market" },
 ];
 
 export default navLinks;
