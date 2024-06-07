5caa09d964a082371f0535b16935055348cd70e6
Justin Lang <91992668+jlang61@users.noreply.github.com>
2024-06-06 20:16:59 -0700

    changing to non prop

--- components/youtube.tsx
+++ components/youtube.tsx
@@ -3,18 +3,13 @@ import React from "react";
 import YouTube from "react-youtube";
 
 // Define the Props interface
-interface YoutubeProps {
-  width: number;
-}
 
-class Youtube extends React.Component<YoutubeProps> {
+
+class Youtube extends React.Component {
   render() {
-    const { width } = this.props;
-    const height = (width / 16) * 9; // Maintain 16:9 aspect ratio
-
     const options = {
-      height: height.toString(),
-      width: width.toString(),
+      height: 255,
+      width: 400,
       playerVars: {
         autoplay: 1 as 0 | 1, // Explicitly define as 0 | 1
         controls: 1 as 0 | 1, // Explicitly define as 0 | 1
