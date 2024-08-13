972c30e3d06d74ebb174176119218e779c5a8a6a
smmzhu <smmzhu@gmail.com>
2024-08-12 23:07:16 -0700

    fixed mild oopsie while fixing linting

--- app/family-trees/page.tsx
+++ app/family-trees/page.tsx
@@ -37,15 +37,15 @@ const NoSSRFamilyTrees = () => {
     "node": () => ({
       className: `px-0 w-12`,
     }),
-    "nodecell": {
+    "nodecell": () => ({
       className: 'px-0',
-    },
-    "line-right": (context: any) => ({
+    }),
+    "line-right": ({context}: any) => ({
       className: `text-center align-top py-0 px-0 rounded-none
                   ${context.lineTop ? 'border-t' : ''}
                   dark:border-blue-900/40`,
     }),
-    "line-left": (context: any) => ({
+    "line-left": ({context}: any) => ({
       className: `text-center align-top py-0 px-0 rounded-none border-r
                   ${context.lineTop ? 'border-t' : ''}
                   dark:border-blue-900/40`,
