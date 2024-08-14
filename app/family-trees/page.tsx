3d245e055a28b9ffd0e5a55de70e4766c2f6f7e2
WinbertZhang <winbertzhang@gmail.com>
2024-08-13 19:12:04 -0700

    Fam Tree Visual Updates

--- app/family-trees/page.tsx
+++ app/family-trees/page.tsx
@@ -1,7 +1,12 @@
 "use client";
-import dynamic from 'next/dynamic'
-import { OrganizationChart } from 'primereact/organizationchart';
-import {assembleTree, getFamNames, recapitalizeAndParseName, yearToRGB} from './parse';
+import dynamic from "next/dynamic";
+import { OrganizationChart } from "primereact/organizationchart";
+import {
+  assembleTree,
+  getFamNames,
+  recapitalizeAndParseName,
+  yearToRGB,
+} from "./parse";
 import styleObj from "./style_obj";
 
 const NoSSRFamilyTrees = () => {
@@ -12,46 +17,63 @@ const NoSSRFamilyTrees = () => {
   for (const famName of famNames) {
     const miniTrees = assembleTree(famName);
     const treeRow = [];
-    
+
     for (const miniTree of miniTrees) {
       treeRow.push([miniTree]);
     }
-    
+
     testTrees.push(treeRow);
   }
 
   // Node template for the organization chart
   const nodeTemplate = (node: any) => {
-    const [firstName, lastName] = recapitalizeAndParseName(node.data.name).split(" ");
+    const [firstName, lastName] = recapitalizeAndParseName(
+      node.data.name
+    ).split(" ");
     const color = yearToRGB(node.data.year);
     return (
-      <div className= {`flex flex-col items-center p-2 ${color} rounded-xl`}>
-          <p className="text-[0.7rem] font-bold">{firstName}</p>
-          <p className="text-[0.7rem] font-bold">{lastName}</p>
-          <p className="text-[0.7rem]">{node.data.year != "None" ? node.data.year : null}</p>
+      <div
+        className={`flex flex-col items-center p-2 ${color} rounded-xl min-w-[5rem]`}
+      >
+        <p className="text-[0.7rem] font-bold">{firstName}</p>
+        <p className="text-[0.7rem] font-bold">{lastName}</p>
+        <p className="text-[0.7rem]">
+          {node.data.year != "None" ? node.data.year : null}
+        </p>
       </div>
     );
   };
 
   return (
     <>
-    <div className = "flex flex-col flex-wrap items-center align-self bg-gray-100">
-      
-      {testTrees.map((treeRow, index) => {
-        return (
-          <div className = "bg-gray-200 flex-auto m-6 p-6 rounded-3xl drop-shadow-lg">
-            <h2 className="text-4xl mb-6 font-semibold text-center"> {recapitalizeAndParseName(famNames[index])} </h2>
-            <div className = "flex flex-row justify-center flex-wrap my-4 ">
-              {treeRow.map((tree, index) => {
-                return (
-                  <OrganizationChart value={tree} nodeTemplate={nodeTemplate} pt = {styleObj}/>
-                )
-              })}
+      <div className="flex flex-col p-8 items-center bg-gray-100 border-black">
+        {testTrees.map((treeRow, index) => {
+          return (
+            <div
+              key={index}
+              className="bg-gray-200 flex-auto m-6 p-6 rounded-3xl drop-shadow-lg w-full"
+            >
+              <h2 className="text-4xl mb-6 font-semibold text-center">
+                {recapitalizeAndParseName(famNames[index])}
+              </h2>
+              <div className="flex flex-row gap-6 my-4 overflow-x-auto overflow-y-hidden">
+                {treeRow.map((tree, idx) => {
+                  return (
+                    <div key={idx} className="flex justify-center">
+                        <OrganizationChart
+                          value={tree}
+                          nodeTemplate={nodeTemplate}
+                          pt={styleObj}
+                          togglerIcon
+                        />
+                      </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        )
-      })}
-    </div>
+          );
+        })}
+      </div>
     </>
   );
 };
--- app/family-trees/style_obj.ts
+++ app/family-trees/style_obj.ts
@@ -8,18 +8,28 @@ const styleObj = {
     "nodecell": () => ({
       className: 'px-0',
     }),
-    "line-right": ({context}: any) => ({
+    'line-right': ({ context }: any) => ({
       className: `text-center align-top py-0 px-0 rounded-none
-                  ${context.lineTop ? 'border-t' : ''}
-                  dark:border-blue-900/40`,
+                  ${context.lineTop ? 'border-t border-black' : ''}
+                  dark:border-blue-900/40
+                  last:border-r-0`,
+  }),
+    'line-left': ({ context }: any) => ({
+        className: `text-center align-top py-0 px-0 rounded-none
+                    ${context.lineTop ? 'border-t border-black' : ''}
+                    border-r border-black dark:border-blue-900/40`,
     }),
-    "line-left": ({context}: any) => ({
-      className: `text-center align-top py-0 px-0 rounded-none border-r
-                  ${context.lineTop ? 'border-t' : ''}
-                  dark:border-blue-900/40`,
+    'line-down': ({ context }: any) => ({
+        className: `border-r border-black dark:border-blue-900/40 `
     }),
-    "line-down": {
-      className: 'border-r dark:border-blue-900/40',
-    },
+    linecell: () => ({
+        className: 'text-center align-top py-0 px-0',
+    }),
+    nodetoggler: () => ({
+        className: `absolute select-none`,
+    }),
+    nodetogglericon: () => ({
+        className: 'relative inline-block w-4 h-4',
+  }),
 } 
 export default styleObj;
