8e4b820757aa80f8570947b5e45b9313ddf2c35a
Justin Lang <jlang61@ucsb.edu>
2024-11-20 01:52:30 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,32 +1,230 @@
 const events = 
 [
   {
-    "title": "Uncle Sam's Tea Time",
-    "day of week": "Tuesday",
-    "date": "2024-11-19",
-    "time": "7-9 PM",
-    "location": "Arts 1356",
+    "title": "Speed Dating",
+    "day of week": "Friday",
+    "date": "2024-01-19",
+    "time": "8-9:30PM",
+    "location": "Lot 22/ Phelps 1160",
     "customTime": "",
     "ready": "Yes"
   },
-  // {
-  //   "title": "Karoke Night",
-  //   "day of week": "Thursday",
-  //   "date": "2024-11-21",
-  //   "time": "8-10 PM",
-  //   "location": "Arts 1353",
-  //   "customTime": "",
-  //   "ready": "Yes"
-  // },
   {
-    "title": "Fall Banquet",
-    "day of week": "Saturday",
-    "date": "2024-11-23",
-    "time": "7-10 PM",
-    "location": "SRB MPR",
+    "title": "GM #1",
+    "day of week": "Monday",
+    "date": "2024-01-22",
+    "time": "8-10PM",
+    "location": "TD-W 1701",
     "customTime": "",
     "ready": "Yes"
   },
+  {
+    "title": "Field Day",
+    "day of week": "Friday",
+    "date": "2024-01-26",
+    "time": "3-5PM",
+    "location": "Student Health Lawn",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TFTI",
+    "day of week": "Saturday",
+    "date": "2024-01-27",
+    "time": "Overnight",
+    "location": "UCLA (Mesa Lawn)",
+    "customTime": "Overnight",
+    "ready": "Yes"
+  },
+  {
+    "title": "Winter Big/Little Reveal",
+    "day of week": "Sunday",
+    "date": "2024-02-04",
+    "time": "2-4PM",
+    "location": "Top of Lot 22",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "GM #2",
+    "day of week": "Monday",
+    "date": "2024-02-05",
+    "time": "8-10PM",
+    "location": "TD-W 1701",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Online Game Night\n",
+    "day of week": "Wednesday",
+    "date": "2024-02-07",
+    "time": "8:00 PM",
+    "location": "Online (Discord)",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Mahjong Mocktails",
+    "day of week": "Thursday",
+    "date": "2024-02-08",
+    "time": "8-10PM ",
+    "location": "Girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "NSU x TASA",
+    "day of week": "Friday",
+    "date": "2024-02-09",
+    "time": "5-7; 9-12AM",
+    "location": "Girvetz 1004",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Lunar New Year Hotpot Night!",
+    "day of week": "Saturday",
+    "date": "2024-02-10",
+    "time": "6-9PM",
+    "location": "Multi Cultural Center",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-02-15",
+    "time": "8-10PM",
+    "location": "Girvetz 1116",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "GM #1 w/ Hochaya",
+    "day of week": "Monday",
+    "date": "2024-04-08",
+    "time": "8-10PM",
+    "location": "TD-W 1701",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-04-11",
+    "time": "8-10PM",
+    "location": "Girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Mahjong Night",
+    "day of week": "Friday",
+    "date": "2024-04-12",
+    "time": "6-8PM",
+    "location": "Arts 1349",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Edible Campus Collab",
+    "day of week": "Saturday",
+    "date": "2024-04-13",
+    "time": "2-3PM",
+    "location": "St. Michael\u2019s University Church (6586 Picasso Rd)",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Maid Cafe",
+    "day of week": "Thursday",
+    "date": "2024-04-18",
+    "time": "6:30- 8:30PM",
+    "location": "ARTS 1356",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "VSA x TASA Collab",
+    "day of week": "Friday",
+    "date": "2024-04-19",
+    "time": "5-7PM",
+    "location": "ARTS 1349",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-04-25",
+    "time": "8-10 PM",
+    "location": "girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Night Market",
+    "day of week": "Saturday",
+    "date": "2024-05-04",
+    "time": "4-8PM",
+    "location": "Storke Plaza",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-05-09",
+    "time": "8-10 PM",
+    "location": "Girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "GM #3: Elections",
+    "day of week": "Monday",
+    "date": "2024-05-20",
+    "time": "8-10 PM",
+    "location": "TD-W 1701",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-05-23",
+    "time": "8-10 PM",
+    "location": "Girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Banquet",
+    "day of week": "Thursday",
+    "date": "2024-05-30",
+    "time": "7-9 PM",
+    "location": "Mosher Alumni House",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "Senior Appreciation",
+    "day of week": "Friday",
+    "date": "2024-05-31",
+    "time": "4-6 PM",
+    "location": "ARTS 1353",
+    "customTime": "",
+    "ready": "Yes"
+  },
+  {
+    "title": "TASA Talks",
+    "day of week": "Thursday",
+    "date": "2024-06-06",
+    "time": "8-10 PM",
+    "location": "Girvetz 1115",
+    "customTime": "",
+    "ready": "Yes"
+  }
 ];
 
 export default events; 
