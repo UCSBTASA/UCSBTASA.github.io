c4dd574997906279bd20f9a6c2c26c972b548d2d
amanda <limamandaqbi@gmail.com>
2025-01-12 21:45:12 -0800

    staff members aligned by the start of their item now - previously, staff members with more lines in their description than others would have photos and names out of alignment

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -1,6 +1,8 @@
 import ScrollTransition from "@/components/scroll-transition";
 import MemberCoin from "../../components/member_coin";
 import staffMembers from "@/data/staffMembers";
+//temp
+import "./staff.css";
 
 const StaffPage = () => {
   return (
@@ -10,7 +12,7 @@ const StaffPage = () => {
           <h2 className="text-3xl font-bold text-gray-900 uppercase">
             Meet the Staff!
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-16 mt-16 mx-16">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-16 mt-16 mx-16 items-start">
             {staffMembers.map((staff) => (
                 <div
                   key={staff.id}
