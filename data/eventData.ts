61cf0b6574466643ece6149231f523bf03b58083
Justin Lang <jlang61@ucsb.edu>
2025-01-28 04:47:39 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -3,7 +3,7 @@ const events =
   {
     "title": "Winter GM #1",
     "day of week": "Monday ",
-    "date": "2025-1-6",
+    "date": "2025-01-06",
     "time": "8-10 PM ",
     "location": "Buchanan 1910 ",
     "customTime": "",
@@ -12,7 +12,7 @@ const events =
   {
     "title": "Winter Retreat",
     "day of week": "Friday ",
-    "date": "2025-1-10",
+    "date": "2025-01-10",
     "time": "Overnight ",
     "location": "Frazier Park ",
     "customTime": "Overnight ",
@@ -21,7 +21,7 @@ const events =
   {
     "title": "Touching Grass w/ TASA ",
     "day of week": "Saturday ",
-    "date": "2025-1-18",
+    "date": "2025-01-18",
     "time": "12-2PM ",
     "location": "Manzi Lawn ",
     "customTime": "",
@@ -30,7 +30,7 @@ const events =
   {
     "title": "Fam Trivia Night ",
     "day of week": "Tuesday ",
-    "date": "2025-1-21",
+    "date": "2025-01-21",
     "time": "7-9 PM ",
     "location": "Buchanan 1930",
     "customTime": "",
@@ -39,7 +39,7 @@ const events =
   {
     "title": "Lunar New Year Event",
     "day of week": "Friday ",
-    "date": "2025-1-24",
+    "date": "2025-01-24",
     "time": "7-9PM ",
     "location": "Girvetz 1004 ",
     "customTime": "",
@@ -48,45 +48,63 @@ const events =
   {
     "title": "Winter GM #2",
     "day of week": "Monday ",
-    "date": "2025-2-3",
+    "date": "2025-02-03",
     "time": "8-10 PM ",
     "location": "Buchanan 1910 ",
     "customTime": "",
-    "ready": "YES "
+    "ready": "YES"
   },
   {
     "title": "Winter Reveal ",
     "day of week": "Saturday ",
-    "date": "2025-2-1",
+    "date": "2025-02-01",
     "time": "2:30-5PM ",
     "location": "Lot 22",
     "customTime": "",
     "ready": "YES"
   },
   {
+    "title": "Pardall Fundraiser",
+    "day of week": "Thursday ",
+    "date": "2025-02-06",
+    "time": "12-3PM",
+    "location": "Lot 22 Lawn",
+    "customTime": "",
+    "ready": "YES"
+  },
+  {
     "title": "Mahjong Night ",
     "day of week": "Friday ",
-    "date": "2025-2-7",
+    "date": "2025-02-07",
     "time": "8-10 PM ",
     "location": "SRB Multipurpose Room",
     "customTime": "",
-    "ready": "YES "
+    "ready": "YES"
   },
   {
-    "title": "Winter GM #3",
-    "day of week": "Monday ",
-    "date": "2025-2-10",
-    "time": "8-10 PM ",
-    "location": "Buchanan 1910 ",
+    "title": "Fam Olympics ",
+    "day of week": "Saturday ",
+    "date": "2025-02-08",
+    "time": "1-3 PM ",
+    "location": "Student Health Lawn",
     "customTime": "",
     "ready": "YES"
   },
   {
     "title": "Green Onion Pancake Workshop ",
     "day of week": "Tuesday ",
-    "date": "2025-2-11",
+    "date": "2025-02-11",
     "time": "3-4 PM ",
     "location": "Methodist Community Garden ",
+    "customTime": "",
+    "ready": "YES"
+  },
+  {
+    "title": "Winter GM #3",
+    "day of week": "Monday ",
+    "date": "2025-02-24",
+    "time": "8-10 PM ",
+    "location": "Buchanan 1910 ",
     "customTime": "",
     "ready": "YES"
   }
