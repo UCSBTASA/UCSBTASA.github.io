23cf8bac756613c787460ed46f80563a2289eb37
Justin Lang <91992668+jlang61@users.noreply.github.com>
2025-02-06 17:30:24 -0800

    adding eventdata

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,76 +1,44 @@
 const events = 
 [
   {
-    "title": "Winter GM #1",
-    "day of week": "Monday ",
-    "date": "2025-1-6",
-    "time": "8-10 PM ",
-    "location": "Buchanan 1910 ",
-    "customTime": "",
-    "ready": "YES"
+    "title": "mahjong night",
+    "day of week": "Friday",
+    "date": "2025-02-07",
+    "time": "08:00 PM - 10:00 PM",
+    "end": "10:00 PM",
+    "start": "08:00 PM",
+    "location": "location tbd",
+    "customTime": ""
   },
   {
-    "title": "Winter Retreat",
-    "day of week": "Friday ",
-    "date": "2025-1-10",
-    "time": "Overnight ",
-    "location": "Frazier Park ",
-    "customTime": "Overnight ",
-    "ready": "YES"
+    "title": "fam olympics",
+    "day of week": "Saturday",
+    "date": "2025-02-08",
+    "time": "01:00 PM - 03:00 PM",
+    "end": "03:00 PM",
+    "start": "01:00 PM",
+    "location": "student health lawn",
+    "customTime": ""
   },
   {
-    "title": "Touching Grass w/ TASA ",
-    "day of week": "Saturday ",
-    "date": "2025-1-18",
-    "time": "12-2PM ",
-    "location": "Manzi Lawn ",
-    "customTime": "",
-    "ready": "YES "
+    "title": "gm 3 !!",
+    "day of week": "Monday",
+    "date": "2025-02-24",
+    "time": "08:00 PM - 10:00 PM",
+    "end": "10:00 PM",
+    "start": "08:00 PM",
+    "location": "location tbd",
+    "customTime": ""
   },
   {
-    "title": "Fam Trivia Night ",
-    "day of week": "Tuesday ",
-    "date": "2025-1-21",
-    "time": "7-9 PM ",
-    "location": "Buchanan 1930",
-    "customTime": "",
-    "ready": "YES"
-  },
-  {
-    "title": "Lunar New Year Event",
-    "day of week": "Friday ",
-    "date": "2025-1-24",
-    "time": "7-9PM ",
-    "location": "Girvetz 1004 ",
-    "customTime": "",
-    "ready": "YEs"
-  },
-  {
-    "title": "Winter GM #2",
-    "day of week": "Monday ",
-    "date": "2025-1-27",
-    "time": "8-10 PM ",
-    "location": "Buchanan 1910 ",
-    "customTime": "",
-    "ready": "YES "
-  },
-  {
-    "title": "Winter Reveal ",
-    "day of week": "Saturday ",
-    "date": "2025-2-1",
-    "time": "2:30-5PM ",
-    "location": "Lot 2 ",
-    "customTime": "",
-    "ready": "YES"
-  },
-  {
-    "title": "Mahjong Night ",
-    "day of week": "Friday ",
-    "date": "2025-2-7",
-    "time": "8-10 PM ",
-    "location": "TBD ",
-    "customTime": "",
-    "ready": "YES "
+    "title": "green onion pancake workshop",
+    "day of week": "Tuesday",
+    "date": "2025-02-25",
+    "time": "03:00 PM - 04:00 PM",
+    "end": "04:00 PM",
+    "start": "03:00 PM",
+    "location": "methodist community garden",
+    "customTime": ""
   }
 ];
 
