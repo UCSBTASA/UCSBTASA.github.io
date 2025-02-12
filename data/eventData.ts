d4fd7212084a9e052642329a63c7148cabf8607a
Justin Lang <jlang61@ucsb.edu>
2025-02-12 22:13:18 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,23 +1,23 @@
 const events = 
 [
   {
-    "title": "mahjong night",
-    "day of week": "Friday",
-    "date": "2025-02-07",
-    "time": "08:00 PM - 10:00 PM",
-    "end": "10:00 PM",
-    "start": "08:00 PM",
-    "location": "location tbd",
+    "title": "taste of taiwan",
+    "day of week": "Tuesday",
+    "date": "2025-02-18",
+    "time": "06:00 PM - 07:30 PM",
+    "end": "07:30 PM",
+    "start": "06:00 PM",
+    "location": "mcc lounge",
     "customTime": ""
   },
   {
-    "title": "fam olympics",
-    "day of week": "Saturday",
-    "date": "2025-02-08",
-    "time": "01:00 PM - 03:00 PM",
-    "end": "03:00 PM",
-    "start": "01:00 PM",
-    "location": "student health lawn",
+    "title": "tasa x nsu collab",
+    "day of week": "Friday",
+    "date": "2025-02-21",
+    "time": "04:00 PM - 06:00 PM",
+    "end": "06:00 PM",
+    "start": "04:00 PM",
+    "location": "srb mpr",
     "customTime": ""
   },
   {
@@ -38,6 +38,26 @@ const events =
     "end": "04:00 PM",
     "start": "03:00 PM",
     "location": "methodist community garden",
+    "customTime": ""
+  },
+  {
+    "title": "karaoke night",
+    "day of week": "Thursday",
+    "date": "2025-02-27",
+    "time": "08:00 PM - 10:30 PM",
+    "end": "10:30 PM",
+    "start": "08:00 PM",
+    "location": "arts 1353",
+    "customTime": ""
+  },
+  {
+    "title": "study night!",
+    "day of week": "Wednesday",
+    "date": "2025-03-12",
+    "time": "08:00 PM - 10:00 PM",
+    "end": "10:00 PM",
+    "start": "08:00 PM",
+    "location": "arts 1349",
     "customTime": ""
   }
 ];
