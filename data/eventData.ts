348ff46c3e99a979f3a9305abce321dc822ff240
Justin Lang <jlang61@ucsb.edu>
2025-02-28 08:29:58 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,16 +1,6 @@
 const events = 
 [
   {
-    "title": "karaoke night",
-    "day of week": "Thursday",
-    "date": "2025-02-27",
-    "time": "08:00 PM - 10:30 PM",
-    "start": "08:00 PM",
-    "end": "10:30 PM",
-    "location": "arts 1353",
-    "customTime": ""
-  },
-  {
     "title": "study night!",
     "day of week": "Wednesday",
     "date": "2025-03-12",
