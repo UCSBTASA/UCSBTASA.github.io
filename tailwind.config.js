edf922e5567ace54510de1defcd304a458fee61c
Justin Lang <91992668+jlang61@users.noreply.github.com>
2025-02-27 17:58:19 -0800

    entirely removing accordion

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,20 +86,6 @@ module.exports = {
         raleway: ["Raleway", "sans-serif"],
         jacques: ['"Jacques Francois"', 'serif'], 
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
