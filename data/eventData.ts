309e6c1200a2f86c1311fc80e2c561ee33776f90
Justin Lang <jlang61@ucsb.edu>
2025-04-03 08:33:01 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,13 +1,103 @@
 const events = 
 [
   {
-    "title": "study night!",
-    "day of week": "Wednesday",
-    "date": "2025-03-12",
+    "title": "gm 1!",
+    "day of week": "Monday",
+    "date": "2025-04-07",
     "time": "08:00 PM - 10:00 PM",
     "start": "08:00 PM",
     "end": "10:00 PM",
-    "location": "arts 2324",
+    "location": "buchanan 1910",
+    "customTime": ""
+  },
+  {
+    "title": "intern water balloon fundraiser",
+    "day of week": "Wednesday",
+    "date": "2025-04-09",
+    "time": "12:00 PM - 12:30 PM",
+    "start": "12:00 PM",
+    "end": "12:30 PM",
+    "location": "pardall lawn",
+    "customTime": ""
+  },
+  {
+    "title": "mahjong night",
+    "day of week": "Thursday",
+    "date": "2025-04-17",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "arts 1349",
+    "customTime": ""
+  },
+  {
+    "title": "slip n' slide (intern event!)",
+    "day of week": "Saturday",
+    "date": "2025-04-19",
+    "time": "01:30 PM - 03:30 PM",
+    "start": "01:30 PM",
+    "end": "03:30 PM",
+    "location": "san miguel lawn (near stage)",
+    "customTime": ""
+  },
+  {
+    "title": "gm 2!",
+    "day of week": "Monday",
+    "date": "2025-04-21",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "buchanan 1910",
+    "customTime": ""
+  },
+  {
+    "title": "tcc (12-4pm each day)",
+    "day of week": NaN,
+    "date": "TBD",
+    "time": "",
+    "start": "TBD",
+    "end": "TBD",
+    "location": "lot 22 lawn",
+    "customTime": "TBD"
+  },
+  {
+    "title": "\u2727\u02d6\u00b0 night market \u2727\u02d6\u00b0",
+    "day of week": "Saturday",
+    "date": "2025-05-03",
+    "time": "04:00 PM - 08:00 PM",
+    "start": "04:00 PM",
+    "end": "08:00 PM",
+    "location": "storke plaza",
+    "customTime": ""
+  },
+  {
+    "title": "karaoke night",
+    "day of week": "Thursday",
+    "date": "2025-05-08",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "arts 1353",
+    "customTime": ""
+  },
+  {
+    "title": "gm 3 (staff elections)",
+    "day of week": "Monday",
+    "date": "2025-05-12",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "buchanan 1910",
+    "customTime": ""
+  },
+  {
+    "title": "maid cafe (location may change)",
+    "day of week": "Thursday",
+    "date": "2025-05-15",
+    "time": "04:00 PM - 06:00 PM",
+    "start": "04:00 PM",
+    "end": "06:00 PM",
+    "location": "hssb 1228",
     "customTime": ""
   }
 ];
