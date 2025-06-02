8181a5f0033ee304090ea20641b6396f5ad7c911
Justin Lang <jlang61@ucsb.edu>
2025-06-02 08:36:16 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,16 +1,6 @@
 const events = 
 [
   {
-    "title": "spring banquet",
-    "day of week": "Sunday",
-    "date": "2025-06-01",
-    "time": "07:00 PM - 10:00 PM",
-    "start": "07:00 PM",
-    "end": "10:00 PM",
-    "location": "iv community center",
-    "customTime": ""
-  },
-  {
     "title": "study night",
     "day of week": "Wednesday",
     "date": "2025-06-04",
