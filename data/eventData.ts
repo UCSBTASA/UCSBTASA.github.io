c5daeedd690219c7039f664ec1ede4a866ba8c3f
Justin Lang <jlang61@ucsb.edu>
2025-09-18 20:27:44 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,13 +1,53 @@
 const events = 
 [
   {
-    "title": "study jam",
-    "day of week": "Wednesday",
-    "date": "2025-06-04",
-    "time": "07:00 PM - 11:00 PM",
+    "title": "campus involvement fair",
+    "day of week": "Tuesday",
+    "date": "2025-09-23",
+    "time": "10:00 AM - 01:00 PM",
+    "start": "10:00 AM",
+    "end": "01:00 PM",
+    "location": "chem lawn",
+    "customTime": ""
+  },
+  {
+    "title": "everything everywhere at ucsb",
+    "day of week": "Tuesday",
+    "date": "2025-09-23",
+    "time": "03:00 PM - 06:00 PM",
+    "start": "03:00 PM",
+    "end": "06:00 PM",
+    "location": "hssb courtyard",
+    "customTime": ""
+  },
+  {
+    "title": "mid-autumn festival",
+    "day of week": "Friday",
+    "date": "2025-10-03",
+    "time": "04:00 PM - 06:00 PM",
+    "start": "04:00 PM",
+    "end": "06:00 PM",
+    "location": "tbd",
+    "customTime": ""
+  },
+  {
+    "title": "GM #1",
+    "day of week": "Monday",
+    "date": "2025-10-06",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "tbd",
+    "customTime": ""
+  },
+  {
+    "title": "speed dating: campfire connections",
+    "day of week": "Friday",
+    "date": "2025-10-10",
+    "time": "07:00 PM - 09:00 PM",
     "start": "07:00 PM",
-    "end": "11:00 PM",
-    "location": "arts 1353",
+    "end": "09:00 PM",
+    "location": "tbd",
     "customTime": ""
   }
 ];
