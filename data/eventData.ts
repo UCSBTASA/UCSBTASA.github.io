f5fcb84233829ea73a06a742b7dbeff8bc22d510
Justin Lang <jlang61@ucsb.edu>
2025-10-07 01:55:45 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -17,7 +17,27 @@ const events =
     "time": "07:00 PM - 09:00 PM",
     "start": "07:00 PM",
     "end": "09:00 PM",
-    "location": "tbd",
+    "location": "top of lot 22",
+    "customTime": ""
+  },
+  {
+    "title": "boba, brushes, & bracelets",
+    "day of week": "Wednesday",
+    "date": "2025-10-15",
+    "time": "07:00 PM - 09:00 PM",
+    "start": "07:00 PM",
+    "end": "09:00 PM",
+    "location": "buchn 1910",
+    "customTime": ""
+  },
+  {
+    "title": "sandy sports day",
+    "day of week": "Sunday",
+    "date": "2025-10-19",
+    "time": "01:00 PM - 03:00 PM",
+    "start": "01:00 PM",
+    "end": "03:00 PM",
+    "location": "meet at san nic, walk to goleta beach",
     "customTime": ""
   },
   {
@@ -28,6 +48,56 @@ const events =
     "start": "08:00 PM",
     "end": "10:00 PM",
     "location": "broida 1610",
+    "customTime": ""
+  },
+  {
+    "title": "camp tasa game night",
+    "day of week": "Thursday",
+    "date": "2025-10-23",
+    "time": "07:00 PM - 09:00 PM",
+    "start": "07:00 PM",
+    "end": "09:00 PM",
+    "location": "srb mpr",
+    "customTime": ""
+  },
+  {
+    "title": "intern panel",
+    "day of week": "Tuesday",
+    "date": "2025-10-28",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "arts 1349",
+    "customTime": ""
+  },
+  {
+    "title": "beachside trailblazers",
+    "day of week": "Wednesday",
+    "date": "2025-10-29",
+    "time": "05:00 PM - 07:00 PM",
+    "start": "05:00 PM",
+    "end": "07:00 PM",
+    "location": "manzi beach",
+    "customTime": ""
+  },
+  {
+    "title": "pre intern social",
+    "day of week": "Wednesday",
+    "date": "2025-11-05",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "girvetz 2127",
+    "customTime": ""
+  },
+  {
+    "title": "big/little reveal!!",
+    "day of week": "Saturday",
+    "date": "2025-11-08",
+    "time": "03:00 PM - 05:00 PM",
+    "start": "03:00 PM",
+    "end": "05:00 PM",
+    "location": "top of lot 22",
     "customTime": ""
   }
 ];
