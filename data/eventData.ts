9b77b362b68cf6fcb34ab6332e350b3cc8d3706d
Justin Lang <jlang61@ucsb.edu>
2025-10-07 08:34:08 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,16 +1,6 @@
 const events = 
 [
   {
-    "title": "GM #1",
-    "day of week": "Monday",
-    "date": "2025-10-06",
-    "time": "08:00 PM - 10:00 PM",
-    "start": "08:00 PM",
-    "end": "10:00 PM",
-    "location": "broida 1610",
-    "customTime": ""
-  },
-  {
     "title": "speed dating: campfire connections",
     "day of week": "Friday",
     "date": "2025-10-10",
