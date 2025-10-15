99d4362a82eb033aa789ab58ddc0af93f25b5671
Justin Lang <jlang61@ucsb.edu>
2025-10-15 20:29:31 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -61,6 +61,16 @@ const events =
     "customTime": ""
   },
   {
+    "title": "GM #3",
+    "day of week": "Monday",
+    "date": "2025-11-03",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "tdw-1701",
+    "customTime": ""
+  },
+  {
     "title": "pre intern social",
     "day of week": "Wednesday",
     "date": "2025-11-05",
