df1c61d98375218723fead1dbbe4ecd7e288c4e1
Justin Lang <jlang61@ucsb.edu>
2025-10-29 16:36:14 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -21,6 +21,16 @@ const events =
     "customTime": ""
   },
   {
+    "title": "panda fundraiser",
+    "day of week": NaN,
+    "date": "TBD",
+    "time": "",
+    "start": "TBD",
+    "end": "TBD",
+    "location": "ucen",
+    "customTime": "TBD"
+  },
+  {
     "title": "pre intern social",
     "day of week": "Wednesday",
     "date": "2025-11-05",
@@ -31,13 +41,43 @@ const events =
     "customTime": ""
   },
   {
+    "title": "mahjong & matcha",
+    "day of week": "Thursday",
+    "date": "2025-11-06",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "arts 1349",
+    "customTime": ""
+  },
+  {
     "title": "big/little reveal!!",
     "day of week": "Saturday",
     "date": "2025-11-08",
-    "time": "03:00 PM - 05:00 PM",
+    "time": "03:00 PM - 05:30 PM",
     "start": "03:00 PM",
-    "end": "05:00 PM",
+    "end": "05:30 PM",
     "location": "top of lot 22",
+    "customTime": ""
+  },
+  {
+    "title": "intern applications close",
+    "day of week": NaN,
+    "date": "TBD",
+    "time": "",
+    "start": "TBD",
+    "end": "TBD",
+    "location": "tbd",
+    "customTime": "TBD"
+  },
+  {
+    "title": "hotpot night!!!",
+    "day of week": "Wednesday",
+    "date": "2025-11-12",
+    "time": "07:00 PM - 09:00 PM",
+    "start": "07:00 PM",
+    "end": "09:00 PM",
+    "location": "tbd",
     "customTime": ""
   }
 ];
