6289c622d4ef71390991984740cef9ac9535121f
Justin Lang <jlang61@ucsb.edu>
2025-11-11 02:06:19 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -9,6 +9,26 @@ const events =
     "end": "09:00 PM",
     "location": "tbd",
     "customTime": ""
+  },
+  {
+    "title": "twins & tea",
+    "day of week": "Saturday",
+    "date": "2025-11-15",
+    "time": "01:00 PM - 03:00 PM",
+    "start": "01:00 PM",
+    "end": "03:00 PM",
+    "location": "arts 1349",
+    "customTime": ""
+  },
+  {
+    "title": "banquet!!!",
+    "day of week": "Saturday",
+    "date": "2025-11-22",
+    "time": "07:00 PM - 09:00 PM",
+    "start": "07:00 PM",
+    "end": "09:00 PM",
+    "location": "tbd",
+    "customTime": ""
   }
 ];
 
