fad92c5f5aeb33f162f1e2db8375968a6a564846
Justin Lang <jlang61@ucsb.edu>
2025-11-13 08:35:41 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,16 +1,6 @@
 const events = 
 [
   {
-    "title": "hotpot night!!!",
-    "day of week": "Wednesday",
-    "date": "2025-11-12",
-    "time": "07:00 PM - 09:00 PM",
-    "start": "07:00 PM",
-    "end": "09:00 PM",
-    "location": "tbd",
-    "customTime": ""
-  },
-  {
     "title": "banquet!!!",
     "day of week": "Saturday",
     "date": "2025-11-22",
