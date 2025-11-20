a7e469f008449cbe42f67e5c7fb2ef95a141c072
Justin Lang <jlang61@ucsb.edu>
2025-11-20 08:37:11 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -7,7 +7,17 @@ const events =
     "time": "07:00 PM - 09:00 PM",
     "start": "07:00 PM",
     "end": "09:00 PM",
-    "location": "tbd",
+    "location": "srb mpr",
+    "customTime": ""
+  },
+  {
+    "title": "study hall",
+    "day of week": "Wednesday",
+    "date": "2025-12-03",
+    "time": "07:00 PM - 11:00 PM",
+    "start": "07:00 PM",
+    "end": "11:00 PM",
+    "location": "girv 2127",
     "customTime": ""
   }
 ];
