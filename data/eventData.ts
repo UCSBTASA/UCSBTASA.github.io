58a7b605f5886e8b3419bd3cddee00fa471ca3a8
Justin Lang <jlang61@ucsb.edu>
2026-03-31 05:48:20 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,13 +1,33 @@
 const events = 
 [
   {
-    "title": "mahjong and mocktails",
+    "title": "ice skating night",
+    "day of week": "Tuesday",
+    "date": "2026-03-31",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "Ice in Paradise",
+    "customTime": ""
+  },
+  {
+    "title": "gm #1 (pie a staff!!)",
+    "day of week": "Monday",
+    "date": "2026-04-06",
+    "time": "07:30 PM - 10:00 PM",
+    "start": "07:30 PM",
+    "end": "10:00 PM",
+    "location": "broida 1610",
+    "customTime": ""
+  },
+  {
+    "title": "bowling night",
     "day of week": "Thursday",
-    "date": "2026-03-05",
-    "time": "07:00 PM - 09:00 PM",
-    "start": "07:00 PM",
-    "end": "09:00 PM",
-    "location": "SRB MPR",
+    "date": "2026-04-09",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "bowlero",
     "customTime": ""
   }
 ];
