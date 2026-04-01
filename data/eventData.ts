fa622e3cbf2816eeb49c6c630317b8a85901bf25
Justin Lang <jlang61@ucsb.edu>
2026-04-01 20:55:57 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -4,8 +4,8 @@ const events =
     "title": "gm #1 (pie a staff!!)",
     "day of week": "Monday",
     "date": "2026-04-06",
-    "time": "07:30 PM - 10:00 PM",
-    "start": "07:30 PM",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
     "end": "10:00 PM",
     "location": "broida 1610",
     "customTime": ""
@@ -18,6 +18,76 @@ const events =
     "start": "08:00 PM",
     "end": "10:00 PM",
     "location": "bowlero",
+    "customTime": ""
+  },
+  {
+    "title": "casino night",
+    "day of week": "Sunday",
+    "date": "2026-04-12",
+    "time": "07:00 PM - 09:30 PM",
+    "start": "07:00 PM",
+    "end": "09:30 PM",
+    "location": "srb mpr",
+    "customTime": ""
+  },
+  {
+    "title": "TCC",
+    "day of week": "Tuesday",
+    "date": "2026-04-14",
+    "time": "11:00 AM - 02:00 PM",
+    "start": "11:00 AM",
+    "end": "02:00 PM",
+    "location": "lot 22 lawn",
+    "customTime": ""
+  },
+  {
+    "title": "TCC",
+    "day of week": "Wednesday",
+    "date": "2026-04-15",
+    "time": "11:00 AM - 02:00 PM",
+    "start": "11:00 AM",
+    "end": "02:00 PM",
+    "location": "lot 22 lawn",
+    "customTime": ""
+  },
+  {
+    "title": "NIGHT MARKET!!!!!",
+    "day of week": "Friday",
+    "date": "2026-04-24",
+    "time": "04:00 PM - 08:00 PM",
+    "start": "04:00 PM",
+    "end": "08:00 PM",
+    "location": "tbd",
+    "customTime": ""
+  },
+  {
+    "title": "gm #2",
+    "day of week": "Monday",
+    "date": "2026-04-27",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "broida 1610",
+    "customTime": ""
+  },
+  {
+    "title": "mahjong night",
+    "day of week": "Thursday",
+    "date": "2026-04-30",
+    "time": "08:00 PM - 10:00 PM",
+    "start": "08:00 PM",
+    "end": "10:00 PM",
+    "location": "srb mpr",
+    "customTime": ""
+  },
+  {
+    "title": "fam olympics",
+    "day of week": "Saturday",
+    "date": "2026-05-02",
+    "time": "01:00 PM - 03:00 PM",
+    "start": "01:00 PM",
+    "end": "03:00 PM",
+    "location": "student health lawn",
     "customTime": ""
   }
 ];
