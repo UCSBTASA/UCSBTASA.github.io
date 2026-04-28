9e77ef9a075e1eaab316aa672f11991f88d70d24
Justin Lang <jlang61@ucsb.edu>
2026-04-28 06:38:01 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,16 +1,6 @@
 const events = 
 [
   {
-    "title": "gm #2",
-    "day of week": "Monday",
-    "date": "2026-04-27",
-    "time": "08:00 PM - 10:00 PM",
-    "start": "08:00 PM",
-    "end": "10:00 PM",
-    "location": "broida 1610",
-    "customTime": ""
-  },
-  {
     "title": "mahjong night",
     "day of week": "Thursday",
     "date": "2026-04-30",
