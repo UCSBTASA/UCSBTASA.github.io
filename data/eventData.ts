73290aba601d3ebde6cadf97b52377cb7ee6cb8b
Justin Lang <jlang61@ucsb.edu>
2026-05-07 03:54:06 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,13 +1,73 @@
 const events = 
 [
   {
+    "title": "thrift sale!",
+    "day of week": "Friday",
+    "date": "2026-05-08",
+    "time": "10:00 AM - 01:00 PM",
+    "start": "10:00 AM",
+    "end": "01:00 PM",
+    "location": "lot 22 lawn",
+    "customTime": ""
+  },
+  {
     "title": "karaoke night w hksa!",
     "day of week": "Saturday",
     "date": "2026-05-09",
     "time": "07:30 PM - 09:30 PM",
     "start": "07:30 PM",
     "end": "09:30 PM",
-    "location": "tbd",
+    "location": "srb mpr",
+    "customTime": ""
+  },
+  {
+    "title": "gm #3: general elections",
+    "day of week": "Monday",
+    "date": "2026-05-11",
+    "time": "07:30 PM - 10:00 PM",
+    "start": "07:30 PM",
+    "end": "10:00 PM",
+    "location": "buch 1930",
+    "customTime": ""
+  },
+  {
+    "title": "cold springs hike w kasa",
+    "day of week": "Friday",
+    "date": "2026-05-15",
+    "time": "04:00 PM - 07:00 PM",
+    "start": "04:00 PM",
+    "end": "07:00 PM",
+    "location": "811 E Mountain Dr, Montecito, CA, 93108",
+    "customTime": ""
+  },
+  {
+    "title": "hotpot night",
+    "day of week": "Saturday",
+    "date": "2026-05-16",
+    "time": "07:00 PM - 09:30 PM",
+    "start": "07:00 PM",
+    "end": "09:30 PM",
+    "location": "srb mpr",
+    "customTime": ""
+  },
+  {
+    "title": "senior appreciation",
+    "day of week": "Wednesday",
+    "date": "2026-05-27",
+    "time": "07:00 PM - 09:00 PM",
+    "start": "07:00 PM",
+    "end": "09:00 PM",
+    "location": "webb 1100",
+    "customTime": ""
+  },
+  {
+    "title": "study hall",
+    "day of week": "Wednesday",
+    "date": "2026-06-03",
+    "time": "07:00 PM - 11:00 PM",
+    "start": "07:00 PM",
+    "end": "11:00 PM",
+    "location": "arts 1349",
     "customTime": ""
   }
 ];
