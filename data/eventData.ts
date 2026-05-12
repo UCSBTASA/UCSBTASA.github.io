8bdc1fd959bfbcbd8f45d661510114614dca9c99
Justin Lang <jlang61@ucsb.edu>
2026-05-12 18:07:23 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -21,6 +21,16 @@ const events =
     "customTime": ""
   },
   {
+    "title": "tea and calligraphy night",
+    "day of week": "Thursday",
+    "date": "2026-05-21",
+    "time": "07:00 PM - 09:00 PM",
+    "start": "07:00 PM",
+    "end": "09:00 PM",
+    "location": "arts 1353",
+    "customTime": ""
+  },
+  {
     "title": "senior appreciation",
     "day of week": "Wednesday",
     "date": "2026-05-27",
