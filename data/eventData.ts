6acadf8004496a86e2b0f153505e0b19ca444f70
Justin Lang <jlang61@ucsb.edu>
2026-05-29 04:24:35 +0000

    Update eventData.ts

--- data/eventData.ts
+++ data/eventData.ts
@@ -1,16 +1,6 @@
 const events = 
 [
   {
-    "title": "movie & popcorn night w hksa!",
-    "day of week": "Thursday",
-    "date": "2026-05-28",
-    "time": "07:00 PM - 09:00 PM",
-    "start": "07:00 PM",
-    "end": "09:00 PM",
-    "location": "buch 1920",
-    "customTime": ""
-  },
-  {
     "title": "study hall",
     "day of week": "Wednesday",
     "date": "2026-06-03",
